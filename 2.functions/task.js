8e5e04613573bfca313c98ce76e2946d3e415def
yu-smirnova <0387097@gmail.com>
2021-09-28 09:11:40 +0300

    homework corrected

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -22,7 +22,7 @@ function getArrayParams(arr) {
   arrParams.max = max;
   arrParams.avg = avg;
 
-  return { min:min, max:max, avg:avg };
+  return arrParams;
 }
 
 // Задание 2
@@ -40,27 +40,17 @@ function makeWork(arrOfArr, func) {
   let max = func(arrOfArr[0]);
 
   for(let arr of arrOfArr){
-    if(max < func(arr)){
-      max = func(arr);
+    let result = func(arr);
+    if(max < result){
+      max = result;
     }
   }
   
-  return max
+  return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  let min = arr[0];
-  let max = arr[0];
-  let minMaxDiff = 0;
-  for (let i = 0; i < arr.length; i++){
-    if(min > arr[i]){
-      min = arr[i];
-    }
-    if(max < arr[i]){
-      max = arr[i];
-    }
-  }
-  minMaxDiff = Math.abs(max - min);
-  return minMaxDiff
+  let arrParams = getArrayParams(arr);
+  return Math.abs(arrParams.max - arrParams.min);
 }
