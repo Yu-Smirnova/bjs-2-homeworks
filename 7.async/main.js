4b9de61dd1e06750c06ab89a3302667e5fa112ca
yu-smirnova <0387097@gmail.com>
2021-11-07 11:00:02 +0300

    homework corrected

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,1 +1,8 @@
-// тут вы можете вызывать функции из task.js
+function testCase() {
+  let alarm = new AlarmClock();
+  alarm.addClock("10:40", () => console.log("Wake up!"), 1);
+  alarm.addClock("10:42", () => console.log("Wake up now!"), 2);
+  alarm.addClock("10:42", () => console.log("Wake up right now!"));
+}
+
+testCase();
--- 7.async/task.js
+++ 7.async/task.js
@@ -1,61 +1,70 @@
 class AlarmClock {
-    constructor() {
-      this.alarmCollection = [],
-      this.timerId = null
-    }
-  
-    addClock(time, callback, id) {
-      if(id === undefined) {
-        throw new Error ('ID is not defined');
-      } else if(this.alarmCollection.some(alarm => alarm.id === id)) {
-        console.error(`Alarm with id ${id} already exists`);
-        break;
-      } else {
-        this.alarmCollection.push({time, callback, id});
-      }
-    }
-  
-    removeClock(id) {
-      let alarmToDelete = this.alarmCollection.filter(alarm => alarm.id === id);
-      if (alarmToDelete.lehgth === 0) {
-        return false;
-      } else {
-        this.alarmCollection.splice(this.alarmCollection.findIndexOf(alarmToDelete), 1);
-        return true;
-      }
-    }
-  
-    getCurrentFormattedTime() {
-      let date = new Date() ;
-      return `${date.getHours()}:${date.getMinutes()}`;
-    }
-  
-    checkClock(alarm) {
-      if(alarm.time === this.getCurrentFormattedTime()) {
-        alarm.callback();
-      }
-    }
-  
-    start() {
-      if(this.timerId === null) {
-        this.timerId = setInterval(this.alarmCollection.every(alarm => alarm.callback()));
-      }
-    }
-  
-    stop() {
-      if(this.timerId !== null) {
-        clearIntrval(this.timerId);
-        this.timerId = null;
-      }
-    }
-  
-    printAlarm() {
-      this.alarmCollection.forEach((alarm) => console.log(`Будильник №${alarm.id} заведен на ${alarm.time} часов.`))
-    }
-  
-    clearAlarms() {
-      this.stop();
-      this.alarmCollection = [];
+  constructor() {
+    (this.alarmCollection = []), (this.timerId = null);
+  }
+
+  addClock(time, callback, id) {
+    if (id === undefined) {
+      throw new Error("ID is not defined");
+    } else if (this.alarmCollection.some((alarm) => alarm.id === id)) {
+      console.error(`Alarm with id ${id} already exists`);
+    } else {
+      this.alarmCollection.push({ time, callback, id });
     }
   }
-  
+
+  removeClock(id) {
+    let index = this.alarmCollection.findIndex((alarm) => alarm.id === id);
+    if (index === -1) {
+      return false;
+    } else {
+      this.alarmCollection.splice(index, 1);
+      return true;
+    }
+  }
+
+  getCurrentFormattedTime() {
+    let date = new Date();
+    let hour = `${date.getHours()}`;
+    if (hour.length === 1) {
+      let hour = `0${hour}`;
+    }
+    let minute = `${date.getMinutes()}`;
+    if (minute.length === 1) {
+      let minute = `0${minute}`;
+    }
+    return `${hour}:${minute}`;
+  }
+
+  checkClock(alarm) {
+    if (alarm.time === this.getCurrentFormattedTime()) {
+      alarm.callback();
+    }
+  }
+
+  start() {
+    if (this.timerId === null) {
+      this.timerId = setInterval(() => {
+        this.alarmCollection.every((alarm) => alarm.callback());
+      }, 5000);
+    }
+  }
+
+  stop() {
+    if (this.timerId !== null) {
+      clearInterval(this.timerId);
+      this.timerId = null;
+    }
+  }
+
+  printAlarm() {
+    this.alarmCollection.forEach((alarm) =>
+      console.log(`Будильник №${alarm.id} заведен на ${alarm.time} часов.`)
+    );
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection = [];
+  }
+}
