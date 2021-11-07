6acca7ee77f64471e86d525e77f672204bd628eb
yu-smirnova <0387097@gmail.com>
2021-11-07 11:10:57 +0300

    main.js updated

--- 7.async/main.js
+++ 7.async/main.js
@@ -3,6 +3,19 @@ function testCase() {
   alarm.addClock("10:40", () => console.log("Wake up!"), 1);
   alarm.addClock("10:42", () => console.log("Wake up now!"), 2);
   alarm.addClock("10:42", () => console.log("Wake up right now!"));
+  alarm.addClock(
+    "10:43",
+    () => {
+      console.log("Alarm! Alarm! Alarm!"), alarm.clearAlarms, alarm.printAlarm;
+    },
+    3
+  );
+  alarm.addClock("10:40", () => console.log("Wake up!"), 1);
+  alarm.printAlarm();
+  alarm.start();
+  alarm.removeClock(2);
+  alarm.removeClock(3);
+  alarm.printAlarm();
 }
 
 testCase();
