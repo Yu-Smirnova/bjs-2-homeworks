1c7fafd3852ba44bcea8c3e0ba7f22be15d97aba
yu-smirnova <0387097@gmail.com>
2021-11-14 10:18:54 +0300

    main.js updated

--- 7.async/main.js
+++ 7.async/main.js
@@ -1,20 +1,30 @@
 function testCase() {
   let alarm = new AlarmClock();
   alarm.addClock("10:40", () => console.log("Wake up!"), 1);
-  alarm.addClock("10:42", () => console.log("Wake up now!"), 2);
-  alarm.addClock("10:42", () => console.log("Wake up right now!"));
+  alarm.addClock(
+    "10:42",
+    () => {
+      console.log("Wake up now!"), alarm.removeClock(2);
+    },
+    2
+  );
+  try {
+    alarm.addClock("10:42", () => console.log("Wake up right now!"));
+  } catch (err) {
+    console.log(`Caught an exception! ${err}`);
+  }
   alarm.addClock(
     "10:43",
     () => {
-      console.log("Alarm! Alarm! Alarm!"), alarm.clearAlarms, alarm.printAlarm;
+      console.log("Alarm! Alarm! Alarm!"),
+        alarm.clearAlarms(),
+        alarm.printAlarm();
     },
     3
   );
   alarm.addClock("10:40", () => console.log("Wake up!"), 1);
   alarm.printAlarm();
   alarm.start();
-  alarm.removeClock(2);
-  alarm.removeClock(3);
   alarm.printAlarm();
 }
 
